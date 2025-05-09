c86bfa96ef5f2d3525865fca9de9848bf3f14a3c
Bárbara Lourenço Tavares <babisolua@gmail.com>
2025-05-09 13:01:51 -0300

    refactor: Implementação de generic crud interface pattern

---
+++ src/main/java/me/barbaraDev/service/CrudService.java
@@ -0,0 +1,11 @@
+package me.barbaraDev.service;
+
+
+import java.util.List;
+
+public interface CrudService<ID, T> {
+    T findById(ID id);
+    T create(T entity);
+    List<T> findAll();
+    T update (ID id, T entity);
+}
--- src/main/java/me/barbaraDev/service/UserService.java
+++ src/main/java/me/barbaraDev/service/UserService.java
@@ -2,8 +2,6 @@ package me.barbaraDev.service;
 
 import me.barbaraDev.domain.model.User;
 
-public interface UserService {
+public interface UserService extends CrudService<Long, User>{
 
-    User findById(Long id);
-    User create(User userToCreate);
 }
